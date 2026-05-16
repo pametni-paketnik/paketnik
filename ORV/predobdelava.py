194144c522275321b335c06d1072487ffdd5a65a
Iris Skledar Hudoklin <iris.skledar@student.um.si>
2026-05-16 18:40:13 +0200

    added function zajemi_obraz  and folder settings for saving

--- ORV/predobdelava.py
+++ ORV/predobdelava.py
@@ -2,3 +2,13 @@ import os
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
+
+def zajemi_obraz():
+    # 1. Nastavitve map
+    base_path = "dataset/surovi_podatki"
+    ime_osebe = input("Vnesite ime osebe, ki jo slikate: ").strip().lower()
+    path = os.path.join(base_path, ime_osebe)
+
+
+if __name__ == "__main__":
+    zajemi_obraz()
