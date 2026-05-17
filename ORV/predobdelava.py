fb66c7ffc55aac86d910434942cba6b4169f29d9
Iris Skledar Hudoklin <irisskledar@gmail.com>
2026-05-17 23:51:49 +0200

    adding preobdelaj_podatke function in preobdelava.py

--- ORV/predobdelava.py
+++ ORV/predobdelava.py
@@ -1,3 +1,15 @@
 import cv2
 import os
+import numpy as np
 
+def predobdelaj_podatke():
+    raw_path = "dataset/surovi_podatki"
+    proc_path = "dataset/obdelani_podatki"
+    target_size = (224, 224) #velikost
+
+
+
+
+
+if __name__ == "__main__":
+    predobdelaj_podatke()
