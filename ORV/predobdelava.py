99592cc1dc6f6f8cee7fc25e76b8ed94fb09b809
Iris Skledar Hudoklin <irisskledar@gmail.com>
2026-05-18 16:50:16 +0200

    fixed preperation of photos for augmentation

--- ORV/predobdelava.py
+++ ORV/predobdelava.py
@@ -46,28 +46,45 @@ def predobdelaj_podatke():
     # Preveri če ima vsaka mapa točno ali več kot 30 slik
     vse_ok = True
 
+    for oseba in osebe:
+        pot = os.path.join(raw_path, oseba)
+        st_slik = len([f for f in os.listdir(pot) if f.lower().endswith(('.jpg', '.jpeg', '.png'))])
+        if st_slik < 30:
+            print(f"[!] STOP: Mapa '{oseba}' ima premalo slik ({st_slik}/30). Dopolnite nabor.")
+            vse_ok = False
+    
+    if not vse_ok:
+        return
+
+    print(f"--- Vsi pogoji izpolnjeni. Začenjam generiranje dataseta za {len(osebe)} osebe. ---")
+
+    if not os.path.exists(proc_path):
+        os.makedirs(proc_path)
+
+
     # Gre čez vse mape oseb (iris, nika, ipd.)
-    for oseba in os.listdir(raw_path):
+    for oseba in osebe:
         oseba_raw_dir = os.path.join(raw_path, oseba)
         oseba_proc_dir = os.path.join(proc_path, oseba)
-
-        if not os.path.isdir(oseba_raw_dir):
-            continue
-
         if not os.path.exists(oseba_proc_dir):
             os.makedirs(oseba_proc_dir)
 
-        print(f"\nObdelujem osebo: {oseba}")
+        print(f"\nObdelujem osebo: {oseba}...")
 
         for img_name in os.listdir(oseba_raw_dir):
             img_path = os.path.join(oseba_raw_dir, img_name)
             img = cv2.imread(img_path)
-
             if img is None:
                 continue
 
              # 1. SPREMEMBA VELIKOSTI (Resizing)
-            img_resized = cv2.resize(img, target_size)
+            img = cv2.resize(img, target_size)
+            ime_osnova = os.path.splitext(img_name)[0]
+            
+            # --- NORMALIZACIJA ---
+            img_norm_test = img.astype('float32') / 255.0
+
+            # --- GENERIRANJE VARIACIJ (Augmentacija) --------------------------------------------------------
 
             # 2. PRETVORBA V SIVINE (če rabi Člana 2)
             img_gray = cv2.cvtColor(img_resized, cv2.COLOR_BGR2GRAY)
