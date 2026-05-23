c8371e9311179f9be2ef4c175c313baec2474138
Iris Skledar Hudoklin <iris.skledar@student.um.si>
2026-05-23 22:07:28 +0200

    connected language  + added camera icon

--- PJ/app/src/main/java/com/example/pametnipaketnik/FaceIdActivity.kt
+++ PJ/app/src/main/java/com/example/pametnipaketnik/FaceIdActivity.kt
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import android.widget.Button
+import android.widget.ImageButton
 import android.widget.Toast
 
 
@@ -14,7 +15,7 @@ class FaceIdActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_face_id)
 
-        val captureButton = findViewById<Button>(R.id.btn_capture)
+        val captureButton = findViewById<ImageButton>(R.id.btn_capture)
         captureButton.setOnClickListener {
             // Tukaj bo kasneje logika za CameraX zajem
             Toast.makeText(this, "Obraz zajet!", Toast.LENGTH_SHORT).show()
