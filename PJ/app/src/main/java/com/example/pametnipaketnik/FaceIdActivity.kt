710ab58414cdf975f82fa77943fe0d10f05fc945
Iris Skledar Hudoklin <iris.skledar@student.um.si>
2026-05-23 22:17:15 +0200

    Camera icon color fix

--- PJ/app/src/main/java/com/example/pametnipaketnik/FaceIdActivity.kt
+++ PJ/app/src/main/java/com/example/pametnipaketnik/FaceIdActivity.kt
@@ -13,11 +13,24 @@ import android.widget.Toast
 class FaceIdActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // Edge-to-Edge
+        enableEdgeToEdge()
+
         setContentView(R.layout.activity_face_id)
 
+        //odmiki zaradi statusne vrstice in prekrivanja napisa
+        val rootLayout = findViewById<android.view.View>(android.R.id.content)        // Če nimaš ID-ja na glavnem layoutu, lahko uporabiš: window.decorView.rootView
+        ViewCompat.setOnApplyWindowInsetsListener(rootLayout) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        // Povezava gumb
         val captureButton = findViewById<ImageButton>(R.id.btn_capture)
         captureButton.setOnClickListener {
-            // Tukaj bo kasneje logika za CameraX zajem
+            // Tukaj bo kasneje logika za zajem slike za ORV
             Toast.makeText(this, "Obraz zajet!", Toast.LENGTH_SHORT).show()
         }
     }
