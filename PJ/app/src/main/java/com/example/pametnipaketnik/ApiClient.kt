c9dc1c89b12bcfb29c5e951986d138186ec5757e
Nika Zajesek <nika.zajsek1@student.um.si>
2026-06-07 08:31:27 +0200

    Poročila in spletna aplikacija - popravek

--- PJ/app/src/main/java/com/example/pametnipaketnik/ApiClient.kt
+++ PJ/app/src/main/java/com/example/pametnipaketnik/ApiClient.kt
@@ -5,7 +5,7 @@ import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-object ApiClient {
+object ApiClient  {
     private var retrofit: Retrofit? = null
     fun initializer(baseUrl: String){
         if(retrofit == null) {
